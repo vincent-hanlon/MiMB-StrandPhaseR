e6a4bf89f56e979642c85fdae481469c298d8a9c
Vincent Hanlon <50639126+vincent-hanlon@users.noreply.github.com>
2022-01-04 09:38:16 -0800

    Update materials.R

--- materials.R
+++ materials.R
@@ -1,3 +1,5 @@
+###################### Installing R packages ######################
+
 install.packages(c("devtools","BiocManager"))
 BiocManager::install(c("GenomicRanges","GenomicAlignments", "BSgenome", "BSgenome.Hsapiens.UCSC.hg38", "zoo", "breakpointR"), force = TRUE)
 devtools::install_github("daewoooo/StrandPhaseR")
