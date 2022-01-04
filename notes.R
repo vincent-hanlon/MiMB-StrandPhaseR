646084152bcdf3fe91622468c0330d4cb8a3925a
Vincent Hanlon <50639126+vincent-hanlon@users.noreply.github.com>
2022-01-04 09:31:04 -0800

    Update notes.R

--- notes.R
+++ notes.R
@@ -1,1 +1,3 @@
+################## Installing a particular StrandPhaseR commit, the one used in this chapter ##################
+
 devtools::install_github("daewoooo/StrandPhaseR", ref="3c8d905bf0269b93f4da1e1ca7934887a32db024")
