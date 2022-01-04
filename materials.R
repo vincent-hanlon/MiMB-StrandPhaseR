9a748933573905bee5818ca485fab6cef430fc97
Vincent Hanlon <50639126+vincent-hanlon@users.noreply.github.com>
2022-01-04 09:12:16 -0800

    Create materials.R

---
+++ materials.R
@@ -0,0 +1,3 @@
+install.packages(c("devtools","BiocManager"))
+BiocManager::install(c("GenomicRanges","GenomicAlignments", "BSgenome", "BSgenome.Hsapiens.UCSC.hg38", "zoo", "breakpointR"), force = TRUE)
+devtools::install_github("daewoooo/StrandPhaseR")
