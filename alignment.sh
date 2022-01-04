6a9f3e1fe1828da6589aca8a5645c4510100d7b1
Vincent Hanlon <50639126+vincent-hanlon@users.noreply.github.com>
2022-01-04 09:09:28 -0800

    Create alignment.sh

---
+++ alignment.sh
@@ -0,0 +1,29 @@
+# 1st argument ($1) is the name of the library to process.
+# 2nd argument ($2) is the path to the reference genome.
+
+# Run like this:
+# bash alignment.sh numCPU /path/to/reference/genome.fasta.
+
+# At the moment, this is set up for PE reads, but it can easily be altered for SE reads.
+# Similarly, this script expect FASTQ files ending in "_R1_001.fastq.gz" and "_R2_001.fastq.gz".
+
+for i in *R1_001.fastq.gz
+do
+       name=$(echo $i | sed 's/_R1_001\.fastq\.gz//')
+       bwa mem -t "$1" "$2" "$i" "$name"_R2_001.fastq.gz  > "$name".sam
+done
+
+# compresses, sorts, marks duplicates, and indexes files with aligned sequence reads.
+process() {
+
+        name=$(echo $1 | sed 's/\.sam//')
+
+        samtools view -q10 -f2 -bS "$name".sam | samtools sort -n -o "$name".bam
+        samtools fixmate -m "$name".bam - | samtools sort -o - - | samtools markdup - "$name".sorted.mdup.bam
+        samtools index "$name".sorted.mdup.bam
+
+}
+
+export -f process
+
+parallel --jobs "$1" process ::: *.sam
